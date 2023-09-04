52b2cf14e5b644697261d42eebf1acacc03fc784
Sveta <bdyysht@gmail.com>
2023-09-04 16:33:20 +0300

    Создала репозиторий с домашкой по С# по семирару от 31.08.23

---
+++ Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 < num2)
+{
+    Console.WriteLine($"Число {num2} больше числа {num1}");
+}
+else if (num2<num1)
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+else
+{
+   Console.WriteLine("Числа равны"); 
+}
---
+++ Task04/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+/*Console.WriteLine("Введите три числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (max < num2)
+{
+    max = num2;
+}
+if (max < num3)
+{
+    max = num3;
+}
+else
+{
+   Console.WriteLine("Числа равны"); 
+}
+Console.WriteLine($"Максимум равен {max}");*/
+
+// Решение задачи через цикл
+Console.WriteLine("Введите три числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+for (int i = 0; i < 3; i++)
+{
+    
+}
